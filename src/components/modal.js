44728ec042657dbabd028ca48dc0096e6d0f1eb1
David Thompson <capndavet@gmail.com>
2020-08-24 17:16:31 -0500

    filled out beds page and added modal component

---
+++ src/components/modal.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+import { colors } from '../styles/global'
+import { FaTimes } from 'react-icons/fa'
+
+const showModalAnimation = keyframes`
+    0% { display: none; opacity: 0; } 
+    1% { display: flex; opacity: 0; }
+    100% { opacity: .9; }
+`
+
+const StyledModal = styled.aside`
+    animation: ${showModalAnimation} .37s ease;
+    background-color: ${colors.black};
+    color: ${colors.white};
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    left: 0;
+    opacity: .925;
+    padding: 1rem;
+    position: fixed;
+    top: 0;
+    width: calc(100vw - 2rem);
+    z-index: 9999;
+`
+
+export default function Modal ({ children, closeModal }) {
+  return (
+    <StyledModal>
+      <FaTimes onClick={closeModal} />
+      { children }
+    </StyledModal>
+  )
+}
--- src/pages/beds.js
+++ src/pages/beds.js
@@ -1,33 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { colors } from '../styles/global'
 import useWindowSize from '../hooks/useWindowSize'
+import Modal from '../components/modal'
 
+const Grid = styled.div`
+    display: grid;
+    gap: 1px;
+    grid-template-columns: repeat(${props => props.columns}, ${props => props.itemWidth});
+    grid-template-rows: repeat(${props => props.rows}, ${props => props.itemWidth});
+    line-height: ${({ itemWidth }) => itemWidth};
+    text-align: center;
+    margin: 1rem auto;
+    vertical-align: middle;
+  `
+
+const GridItem = styled.button`
+  background: ${colors.white};
+  border: 1px solid ${colors.primary};
+  border-radius: 5px;
+  margin: 0;
+  text-decoration: none;
+  transition: background 250ms ease-in-out, transform 150ms ease;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  &:hover, :focus {
+    background: ${colors.accent};
+  }
+  `
 function Beds () {
+  const [modalActive, setModalActive] = useState(false)
+
+  const plantChoices = [
+    'Arugula', 'Green Beans', 'Spinach', 'Lettuce', ''
+  ]
   const { width } = useWindowSize()
-  const rows = 2
-  const columns = 3
-  const gridWidth = width > 700 ? 600 : width - 12
+  const rows = 4
+  const columns = 5
+
+  /* For mobile, width - 2rem - grid-gap */
+  const gridWidth = (
+    width > 700
+      ? 600
+      : width - 32 - (columns - 1)
+  )
+
   const gridItemWidth = `${gridWidth / columns}px`
 
-  const Grid = styled.div`
-    display: grid;
-    gap: 2px;
-    grid-template-columns: repeat(${columns}, ${gridItemWidth});
-    grid-template-rows: repeat(${rows}, ${gridItemWidth});
-  `
+  /**
+  * Create GridItem for each of row*column
+  */
+  const gridItems = []
+  for (let i = 0; i < rows * columns; i++) {
+    gridItems.push(
+      <GridItem
+        onClick={() => setModalActive(true)}
+      >
+        {i + 1}
+      </GridItem>
+    )
+  }
 
-  const GridItem = styled.div`
-    background: ${colors.accent};
-  `
   return (
-    <Grid>
-      <GridItem>one</GridItem>
-      <GridItem>two</GridItem>
-      <GridItem>three</GridItem>
-      <GridItem>four</GridItem>
-      <GridItem>five</GridItem>
-    </Grid>
+    modalActive
+      ? <Modal closeModal={(event) => {
+        console.log(event)
+        setModalActive(false)
+      }} />
+      : <Grid
+        columns={columns}
+        rows={rows}
+        itemWidth={gridItemWidth}
+      >
+        {gridItems}
+      </Grid>
   )
 }
 
