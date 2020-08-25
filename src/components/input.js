5f294d99d41d1f0665afbdbe1ecdbbe6276d87a2
David Thompson <capndavet@gmail.com>
2020-08-25 15:28:07 -0500

    finished basics of input element

--- src/components/input.js
+++ src/components/input.js
@@ -1,43 +1,191 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { colors } from '../styles/global'
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa'
 
 const StyledInput = styled.input`
   font-family: inherit;
   font-size: 100%;
   height: 3rem;
   border: none;
+  border-image-width: 0;
   border-radius: 5px;
   box-shadow: none !important;
   -webkit-box-shadow: none !important;
   -moz-box-shadow: none !important;
+  padding: 0 0 0 1rem;
 `
 
+const StyledUl = styled.ul`
+  border: 1px solid #999;
+  border-top-width: 0;
+  list-style: none;
+  margin-top: 0;
+  max-height: 250px;
+  overflow-y: auto;
+  padding: .5rem 1rem 1rem;
+  transform: translate(-2px);
+  margin-right: -1rem;
+`
+
+const StyledLi = styled.li`
+  padding-top: .5rem;
+`
+
+const StyledArrowIconDown = styled(FaAngleDown)`
+    color: ${colors.black};
+    font-size: 120%;
+    margin-left: -2rem;
+    margin-bottom: -.25rem;
+  `
+const StyledArrowIconUp = styled(FaAngleUp)`
+    color: ${colors.black};
+    font-size: 120%;
+    margin-left: -2rem;
+    margin-bottom: -.25rem;
+  `
+
+const InputWrapper = styled.div`
+`
+
+/**
+ * Input
+ * @param { options } list of items that will auto-populate as suggestions
+ */
 function Input ({ options }) {
-  const [selectedInput, setSelectedInput] = useState('')
+  const [userInput, setUserInput] = useState(undefined)
+  const [showDropdown, setShowDropdown] = useState(false)
+  const [filteredOptions, setFilteredOptions] = useState([])
+  const [activeOption, setActiveOption] = useState(0)
+  const [arrowIconDirection, setArrowIconDirection] = useState('down')
 
   /**
    * Event that's fired when input changes
    */
   function onChange (event) {
     const userInput = event.target.value
+    setShowDropdown(true)
 
     // filter out suggestions that don't contain input
     const filteredOptions = options.filter(
       option =>
         option.toLowerCase().indexOf(userInput.toLowerCase()) > -1
     )
-
-    setSelectedInput(userInput)
+    setFilteredOptions(filteredOptions)
+    setUserInput(userInput)
   }
 
+  /**
+   *  Event fired when the user clicks on a suggestion
+   */
+  function onClick (event) {
+    setActiveOption(0)
+    setFilteredOptions([])
+    setUserInput(event.target.innerText)
+    setShowDropdown(false)
+  }
+
+  /**
+   * Event fired when user clicks on input
+   */
+  function onArrowClick (event) {
+    if (userInput === undefined || userInput.length === 0) {
+      setUserInput('')
+    }
+    if (filteredOptions.length === 0) {
+      setFilteredOptions(options)
+    }
+    setShowDropdown(!showDropdown)
+    setArrowIconDirection(arrowIconDirection === 'up' ? 'down' : 'up')
+  }
+
+  // Event fired when the user presses a key down
+  function onKeyDown (event) {
+    // Don't let component rerender
+    event.preventDefault()
+
+    // If user pressed enter, update input and close options
+    if (event.keyCode === 13) {
+      setActiveOption(0)
+      setShowDropdown(false)
+      setUserInput(filteredOptions[activeOption])
+    }
+
+    // If user pressed up arrow, decrement the index
+    else if (event.keyCode === 38) {
+      if (activeOption === 0) {
+        return
+      }
+      setActiveOption(activeOption - 1)
+    }
+
+    // If User pressed down arrow, increment the index
+    else if (event.keyCode === 40) {
+      if (activeOption - 1 === filteredOptions.length) {
+        return
+      }
+      setActiveOption(activeOption + 1)
+    }
+  }
+
+  /**
+   * Dropdown component
+   */
+  let dropdown
+
+  if (userInput !== undefined && showDropdown) {
+    // define element
+    dropdown =
+       <StyledUl>
+         {filteredOptions.map((option, index) => {
+           /**
+            * Conditionally add style to element
+            */
+           if (index === activeOption) {
+             const ActiveLi = styled(StyledLi)`
+               color: ${colors.accent};
+             `
+             return (
+               <ActiveLi
+                 key={option}
+                 onClick={onClick}
+               >
+                 {option}
+               </ActiveLi>
+
+             )
+           } else {
+             return (
+               <StyledLi
+                 key={option}
+                 onClick={onClick}
+               >
+                 {option}
+               </StyledLi>
+             )
+           }
+         })}
+       </StyledUl>
+  }
+
+  /**
+   * Get icon which is appropriately up or down
+   */
+  const arrowIcon = arrowIconDirection === 'down'
+    ? <StyledArrowIconDown onClick={onArrowClick}/>
+    : <StyledArrowIconUp onClick={onArrowClick}/>
+
   return (
-    <StyledInput
-      type='text'
-      onChange={onChange}
-      //   onKeyDown={onKeyDown}
-      value={selectedInput}
-    />
+    <InputWrapper>
+      <StyledInput
+        type='text'
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        value={userInput}
+      />
+      {arrowIcon}
+      {dropdown}
+    </InputWrapper>
   )
 }
 
--- src/pages/PlantSelector.js
+++ src/pages/PlantSelector.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 import Input from '../components/input'
+
+const Form = styled.form`
+
+`
 
 function PlantSelector () {
   const plantChoices = [
-    'Arugula', 'Green Beans', 'Spinach', 'Lettuce', ''
+    'Arugula', 'Green Beans', 'Spinach', 'Tomato', 'Tomatillo', 'Lettuce', ''
   ]
 
   return (
-    <Input
-      options={plantChoices}
-    />
+    <Form>
+      <label>
+      Plant Type
+        <Input
+          options={plantChoices}
+        />
+      </label>
+    </Form>
   )
 }
 
