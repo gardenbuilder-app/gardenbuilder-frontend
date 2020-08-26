de39d8b80d56db38e13893cf322d2398372e4503
David Thompson <capndavet@gmail.com>
2020-08-26 14:50:29 -0500

    added apolloclient to project and wrapped app in ApolloProvider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const apolloClient = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_SERVER,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
