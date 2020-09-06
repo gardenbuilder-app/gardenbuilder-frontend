28807be9920f8d197609c827a48fdcb96bf99ffc
David Thompson <dthompson@tcadcentral.org>
2020-09-06 17:41:42 -0500

    added props to pass from Login component to Input and Button components

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,17 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import { Input, Button } from "../../components"
 
 const StyledForm = styled.form``
 
 export function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <StyledForm>
       <label htmlFor="email">Email</label>
-      <Input name="email" />
+      <Input name="email" value={email} setValue={setEmail} />
       <label htmlFor="password">Password</label>
-      <Input name="password" />
-      <Button name="submit" />
+      <Input name="password" value={password} setValue={setPassword} />
+      <Button name="submit" text="Submit" />
     </StyledForm>
   )
 }
