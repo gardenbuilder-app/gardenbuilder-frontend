0601a48efd19385661d5567b516c6935ed2a5716
David Thompson <dthompson@tcadcentral.org>
2020-09-07 10:00:46 -0500

    removed unneccesary React import

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 /**
  * Calculate window size, update on resize
  */
-export default function useWindowSize () {
+export default function useWindowSize() {
   // Initialize state with undefined width/height so server and client renders match
   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
   const [windowSize, setWindowSize] = useState({
     width: undefined,
-    height: undefined
+    height: undefined,
   })
 
   useEffect(() => {
     // Handler to call on window resize
-    function handleResize () {
+    function handleResize() {
       setWindowSize({
         width: window.innerWidth,
-        height: window.innerHeight
+        height: window.innerHeight,
       })
     }
 
     // Add event listener
-    window.addEventListener('resize', handleResize)
+    window.addEventListener("resize", handleResize)
 
     // Call handler right away so state gets updated with initial window size
     handleResize()
 
     // Remove event listener on cleanup
-    return () => window.removeEventListener('resize', handleResize)
+    return () => window.removeEventListener("resize", handleResize)
   }, []) // Empty array ensures that effect is only run on mount
 
   return windowSize
