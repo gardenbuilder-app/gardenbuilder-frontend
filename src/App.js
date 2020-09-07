b062ed5524f8d083accc44449970849edf5d3038
David Thompson <dthompson@tcadcentral.org>
2020-09-07 10:01:37 -0500

    removed unneccesary Beds import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Layout } from './components'
-import { Beds, Login } from './pages'
+import React from "react"
+import { Layout } from "./components"
+import { Login } from "./pages"
 
-function App () {
+function App() {
   return (
     <Layout>
-      <Login options={[]}/>
+      <Login options={[]} />
     </Layout>
   )
 }
