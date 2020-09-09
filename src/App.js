c802b26f2441801063b9d4d409f7ce736261e25c
David Thompson <dthompson@tcadcentral.org>
2020-09-09 11:28:47 -0500

    Added switch and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react"
 import { Layout } from "./components"
-import { Login } from "./pages"
+import { Login, Beds } from "./pages"
+import { Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <Layout>
-      <Login options={[]} />
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/beds" component={Beds} />
+      </Switch>
     </Layout>
   )
 }
