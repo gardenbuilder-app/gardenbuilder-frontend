2b8869f41fde5d7945ec76e0e7156e1d01ff34d3
David Thompson <dthompson@tcadcentral.org>
2020-09-09 10:00:30 -0500

    added useCookie hook

---
+++ src/hooks/useCookie.js
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+export const setCookie = (name, value, options) => {
+  const optionsWithDefaults = {
+    days: 7,
+    path: "/",
+    ...options,
+  }
+  const expires = new Date(
+    Date.now() + optionsWithDefaults.days * 864e5
+  ).toUTCString()
+  document.cookie =
+    name +
+    "=" +
+    encodeURIComponent(value) +
+    "; expires=" +
+    expires +
+    "; path=" +
+    optionsWithDefaults.path
+}
+
+export const getCookie = (name) => {
+  return document.cookie.split("; ").reduce((r, v) => {
+    const parts = v.split("=")
+    return parts[0] === name ? decodeURIComponent(parts[1]) : r
+  }, "")
+}
+
+export default function (key, initialValue) {
+  const [item, setItem] = useState(() => {
+    return getCookie(key) || initialValue
+  })
+
+  const updateItem = (value, options) => {
+    setItem(value)
+    setCookie(key, value, options)
+  }
+
+  return [item, updateItem]
+}
