623e57450df852a7c9ccd496b00e3d621d2a0c5d
David Thompson <dthompson@tcadcentral.org>
2020-09-09 10:03:11 -0500

    changed from default to regular export

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 /**
  * Calculate window size, update on resize
  */
-export default function useWindowSize() {
+export function useWindowSize() {
   // Initialize state with undefined width/height so server and client renders match
   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
   const [windowSize, setWindowSize] = useState({
