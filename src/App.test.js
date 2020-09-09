a124f7124ddc6128fa22fef586704c7c4166c25c
David Thompson <dthompson@tcadcentral.org>
2020-09-09 16:24:25 -0500

    cleaned up tests for App component (gave up on testing cookie > routing

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,22 @@
 import React, { useState } from "react"
 import { render, fireEvent } from "@testing-library/react"
 import { App } from "./App"
-import useCookie from "./hooks"
 import { MemoryRouter, useHistory } from "react-router-dom"
 import { MockedProvider, screen } from "@apollo/client/testing"
 
-describe.only("<App/>", () => {
-  function setup() {
-    return render(
-      <MemoryRouter>
-        <MockedProvider>
-          <App />
-        </MockedProvider>
-      </MemoryRouter>
-    )
-  }
+function setup() {
+  return render(
+    <MemoryRouter>
+      <MockedProvider>
+        <App />
+      </MockedProvider>
+    </MemoryRouter>
+  )
+}
 
+describe.only("<App/>", () => {
   it("should route to root if cookie is not set", () => {
     const container = setup()
     expect(container.getAllByRole("heading")[1].textContent).toBe("Sign Up")
-  })
-
-  it.only("should route to beds if cookie is set", () => {
-    // To Do
-    expect(true).toBe(true)
   })
 })
