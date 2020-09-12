00bbd0c28b6930778224b5afb07bda299b8d2316
David Thompson <dthompson@tcadcentral.org>
2020-09-12 18:09:29 -0500

    made MobileHeader tests

---
+++ src/components/Header/MobileHeader/MobileHeader.test.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react"
+import { render, fireEvent, screen, getByRole, within } from "@testing-library/react"
+import { MobileHeader } from "./MobileHeader"
+
+describe.only("<MobileHeader /> component", () => {
+  const { getByText, getByRole } = render(<MobileHeader />)
+  const button = getByRole("button", { name: /menu/gi })
+  fireEvent.click(button)
+
+  it("should show Profile, Gardens, and Logout in the menu after clicking the hamburger button", async () => {
+    const menuItems = ["Profile", "Gardens", "Logout"]
+    for (const menuItem of menuItems) {
+      const renderedItem = await getByText(menuItem)
+      expect(renderedItem).toBeInTheDocument()
+    }
+  })
+})
