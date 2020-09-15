3850ffcf38e91ecc2b521230bfb0748d6adef94d
David Thompson <capndavet@gmail.com>
2020-09-15 12:09:47 -0500

    ensured cookie being called

--- src/index.js
+++ src/index.js
@@ -3,17 +3,40 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
 import { BrowserRouter } from "react-router-dom"
 
+/* Get token if it exists, add to header, return header */
+const authLink = setContext((_, { headers }) => {
+  const token = document.cookie
+    .split("; ")
+    .find((key) => key.startsWith("gardenbuilder-jwt-token"))
+    ?.split("=")[1]
+
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`,
+    },
+  }
+})
+
+const httpLink = createHttpLink({ uri: process.env.REACT_APP_GRAPHQL_SERVER })
+
 const apolloClient = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_SERVER,
   cache: new InMemoryCache(),
   defaultOptions: {
     mutate: {
       errorPolicy: "all",
     },
   },
+  link: authLink.concat(httpLink),
 })
 
 ReactDOM.render(
