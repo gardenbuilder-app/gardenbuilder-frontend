36c6ed57cca6bcb70fd6416da3d4c9e875db895a
David Thompson <capndavet@gmail.com>
2020-09-15 13:50:16 -0500

    changed alert property to error property

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,9 @@
 export const colors = {
-  white: '#fff',
-  black: '#1f271b',
-  primary: '#71b48d',
-  secondary: '#19647e',
-  background: 'fff7e1',
-  accent: '#86cb92',
-  alert: '#e5446d'
+  white: "#fff",
+  black: "#1f271b",
+  primary: "#71b48d",
+  secondary: "#19647e",
+  background: "fff7e1",
+  accent: "#86cb92",
+  error: "#e5446d",
 }
