36e6969a269a2788c826642c5a95175ed766665c
David Thompson <capndavet@gmail.com>
2020-09-15 12:21:20 -0500

    updated header to correctly send jwt token

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: `Bearer ${token}`,
+      authorization: `JWT ${token}`,
     },
   }
 })
