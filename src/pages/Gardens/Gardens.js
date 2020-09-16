e40f7153cbb0fc85bafd640791bed6edee921357
David Thompson <capndavet@gmail.com>
2020-09-16 17:10:02 -0500

    removed unneccesary variables

--- src/pages/Gardens/Gardens.js
+++ src/pages/Gardens/Gardens.js
@@ -1,13 +1,9 @@
 import React from "react"
-import { gql, useQuery } from "@apollo/client"
+import { useQuery } from "@apollo/client"
 import { GET_USER_GARDENS } from "../../queries/queries"
 
 export function Gardens() {
-  const { data, loading, error } = useQuery(GET_USER_GARDENS, {
-    onError(err) {
-      console.log(err)
-    },
-  })
+  const { data } = useQuery(GET_USER_GARDENS)
 
   const gardenNames = data
     ? data.userGardens.map((garden, index) => (
