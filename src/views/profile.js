dd7aaf7dd4f3518f674f0392bd7930e896e7cb96
David Thompson <capndavet@gmail.com>
2020-09-16 17:38:15 -0500

    moved style to styled-components from css module

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import profileStyles from './profile.module.css'
+import React from "react"
+import styled from "styled-components"
+
+const ProfileStyle = styled.div`
+  height: 50px;
+  width: 50px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+`
 
 const Profile = () => {
-  // needs user, name, picture, email
-  return (
-    <div className={profileStyles.circle}>
-        DT
-      <img></img>
-    </div>
-  )
+  return <ProfileStyle>DT</ProfileStyle>
 }
 
 export default Profile
