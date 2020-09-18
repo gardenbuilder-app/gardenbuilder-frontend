d706d971c74e36562937b585f90f7bb16b7e9231
David Thompson <capndavet@gmail.com>
2020-09-18 09:48:31 -0500

    moved and renamed Profile component

--- src/components/Header/DesktopHeader/DesktopHeader.js
+++ src/components/Header/DesktopHeader/DesktopHeader.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Navbar } from "./Navbar"
-import Profile from "../../../views/profile"
+import Profile from "./Profile"
 
 export const DesktopHeader = function () {
   return (
