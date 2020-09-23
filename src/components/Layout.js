dedbd7039e3d43373d7a8f439c6053e079bf2785
David Thompson <capndavet@gmail.com>
2020-09-23 10:49:01 -0500

    ensured layout is full-page rather then restricted width

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,12 @@
-import React from 'react'
-import { Header } from './Header'
-import { Footer } from './Footer'
-import styled from 'styled-components'
+import React from "react"
+import { Header } from "./Header"
+import { Footer } from "./Footer"
+import styled from "styled-components"
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 0 auto;
-  max-width: 1500px;
   min-height: 100vh;
-  width: calc(100% - 2rem);
 `
 const Main = styled.main`
   flex-grow: 1;
@@ -18,11 +15,9 @@ const Main = styled.main`
 export const Layout = function ({ children }) {
   return (
     <Container>
-      <Header/>
-      <Main>
-        {children}
-      </Main>
-      <Footer/>
+      <Header />
+      <Main>{children}</Main>
+      <Footer />
     </Container>
   )
 }
