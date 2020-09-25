1693a286dd965100f4094686cf3921c471e2c640
David Thompson <capndavet@gmail.com>
2020-09-25 14:56:39 -0500

    wrote basic logout component and (incomplete) test.

--- src/components/Header/Logout/Logout.js
+++ src/components/Header/Logout/Logout.js
@@ -1,9 +1,16 @@
 import React from "react"
+import { useCookie } from "../../../hooks/useCookie"
+import { useHistory } from "react-router-dom"
 
 export function Logout() {
+  const [cookie, setCookie] = useCookie()
+  const history = useHistory()
+
   function handleLogOut() {
     // TODO: remove jwt token from app
+    setCookie("gardenbuilder-jwt-token", "", { days: 0 })
     // TODO: route to login page
+    history.push("/login")
   }
   return <li onClick={handleLogOut}>Log Out</li>
 }
--- src/components/Header/Logout/Logout.test.js
+++ src/components/Header/Logout/Logout.test.js
@@ -1,12 +1,31 @@
 import React, { useState } from "react"
-import { render, fireEvent, screen, getByRole, within } from "@testing-library/react"
+import { render, fireEvent, waitFor, screen } from "@testing-library/react"
 import { Logout } from "./Logout"
+import { MemoryRouter } from "react-router-dom"
+import { getCookie } from "../../../hooks/useCookie"
 
 describe.only("<MobileHeader /> component", () => {
-  const { getByText, getByRole } = render(<Logout />)
+  let container
+  beforeEach(() => {
+    container = render(
+      <MemoryRouter>
+        <Logout />
+      </MemoryRouter>
+    )
+  })
 
   it("should render the words Log Out on the screen", () => {
-    const logoutText = getByText("Log Out")
+    const logoutText = container.getByText("Log Out")
     expect(logoutText).toBeInTheDocument()
+  })
+
+  it("should erase the gardenbuilder-jwt-cookie on click", async () => {
+    // set token
+    document.cookie =
+      "gardenbuilder-jwt-token=abc123; expires=Thu, 18 Dec 2040 12:00:00 UTC"
+    const logoutElement = container.getByText("Log Out")
+    fireEvent.click(logoutElement)
+    // await waitFor(() => expect(getCookie("gardenbuilder-jwt-token")).toBe(undefined))
+    expect(getCookie("gardenbuilder-jwt-token")).toBe(undefined))
   })
 })
