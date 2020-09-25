173621f8d794bd049e24dfe5350d223690ce862d
David Thompson <capndavet@gmail.com>
2020-09-25 16:34:28 -0500

    update cookies file to separately add in the token and expired parameters

--- src/libs/cookies.js
+++ src/libs/cookies.js
@@ -8,3 +8,24 @@ export function getToken() {
     .find((key) => key.startsWith("gardenbuilder-jwt-token"))
     ?.split("=")[1]
 }
+
+/**
+ * Insert JWT token into cookie
+ */
+export function setToken(value, days = 7) {
+  let expirationDate = ""
+  if (days) {
+    const date = new Date()
+    date.setTime(date.getTime() + days * 24 * 1200 * 1000)
+    expirationDate = date.toGMTString()
+  }
+  document.cookie = `gardenbuilder-jwt-token=${value};`
+  document.cookie = `expires=${expirationDate};`
+}
+
+/**
+ * Set cookie value to empty and date to expired
+ */
+export function eraseToken() {
+  setToken("", -1)
+}
