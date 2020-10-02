ebcff8a2ed470b30ce7f2374bd8f39ec6057a8ae
David Thompson <capndavet@gmail.com>
2020-10-02 16:54:07 -0500

    added BedList to Garden component

--- src/pages/Garden/Garden.js
+++ src/pages/Garden/Garden.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { BedList } from "./BedList"
 
 export function Garden() {
   return (
     <div>
       <h2>Garden</h2>
       hi
+      <BedList />
     </div>
   )
 }
