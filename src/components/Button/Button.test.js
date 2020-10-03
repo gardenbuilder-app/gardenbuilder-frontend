327bdd109242f0d9cf3366c161f6fd0547e5f21c
Harpal <harpalassi@gmail.com>
2020-10-03 12:33:51 -0500

    refactor button tests/setup

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { render, fireEvent, screen, getByRole, within } from "@testing-library/react"
+import { render, fireEvent, screen } from "@testing-library/react"
 import { Button } from "./Button"
 
 function ButtonWrapper() {
@@ -7,23 +7,17 @@ function ButtonWrapper() {
   return <Button name="input" text={text} onClick={() => setText("updatedText")} />
 }
 
-describe.only("<Button /> component", () => {
-  function setup() {
-    const { container } = render(<ButtonWrapper />)
-    const button = container.querySelector("button")
-    return button
-  }
+describe("<Button /> component", () => {
+  beforeEach(() => {
+    render(<ButtonWrapper />)
+  })
 
   it("should show text that is passed to it", () => {
-    const button = setup()
-    const { getByText } = within(button)
-    expect(getByText("initialText")).toBeInTheDocument()
+    expect(screen.getByText("initialText")).toBeInTheDocument()
   })
 
   it("should trigger the passed method onClick", () => {
-    const button = setup()
-    fireEvent.click(button)
-    const { getByText } = within(button)
-    expect(getByText("updatedText")).toBeInTheDocument()
+    fireEvent.click(screen.getByRole("button"))
+    expect(screen.getByText("updatedText")).toBeInTheDocument()
   })
 })
