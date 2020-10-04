d4bb017a952a51a460a8551fe0992bb139ec7fa2
luciano.virmes <luciano.virmes18@gmail.com>
2020-10-03 21:37:30 -0300

    added a condition to show menu just if not in login

--- src/components/Header/DesktopHeader/Navbar/Navbar.js
+++ src/components/Header/DesktopHeader/Navbar/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react"
 import styled from "styled-components"
 import { Logout } from "../../Logout"
+import { useLocation } from 'react-router-dom'
 
 const Nav = styled.nav`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  list-style-type: none;
+display: flex;
+flex-direction: row;
+justify-content: space-evenly;
+list-style-type: none;
 `
 
 /**
@@ -15,25 +16,31 @@ const Nav = styled.nav`
  * increase font size if active
  */
 const NavItem = styled.li`
-  cursor: pointer;
-  text-decoration: none;
-  &:hover {
-    opacity: 0.7;
-  }
-  &:active {
-    font-size: 140%;
-  }
+cursor: pointer;
+text-decoration: none;
+&:hover {
+  opacity: 0.7;
+}
+&:active {
+  font-size: 140%;
+}
 `
 
 export function Navbar() {
+
+  const location = useLocation()
+  const showLogout = location.pathname != "/login"
+
   return (
     <Nav>
       <NavItem to="/beds">Beds</NavItem>
       <NavItem to="/sections">Sections</NavItem>
       <NavItem to="/plants">Plants</NavItem>
-      <NavItem>
+      {showLogout ?
         <Logout />
-      </NavItem>
+        :
+        ""
+      }
     </Nav>
   )
 }
