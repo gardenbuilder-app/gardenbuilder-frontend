7df2f2c07d14adc9d20a42b9067733aa22941a2d
David Thompson <capndavet@gmail.com>
2020-10-09 15:22:44 -0500

    moved ProfileIcon to archive (out of UI)

--- src/components/Header/DesktopHeader/archive/ProfileIcon.js
+++ src/components/Header/DesktopHeader/archive/ProfileIcon.js
@@ -1,22 +1,23 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import styled from "styled-components"
-import useUser from "../../../hooks/useUser"
 
-const ProfileIconWrapper = styled.div`
+const ProfileIconWrapper = styled(Link)`
   align-items: center;
   background-color: white;
+  border 3px solid black;
   border-radius: 50%;
+  color: black;
   display: flex;
   height: 50px;
   justify-content: center;
+  text-decoration: none;
   width: 50px;
+  &:hover {
+    opacity: .8;
+  }
 `
 
 export const ProfileIcon = () => {
-  const loggedInUser = useUser()
-  return (
-    <ProfileIconWrapper>
-      {loggedInUser.email.substring(0, 1).toUpperCase()}
-    </ProfileIconWrapper>
-  )
+  return <ProfileIconWrapper to="/profile">Profile</ProfileIconWrapper>
 }
