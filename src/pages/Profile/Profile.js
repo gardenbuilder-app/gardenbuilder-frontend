69b2c345021716123ccb477b268366a24398076b
David Thompson <capndavet@gmail.com>
2020-10-09 10:56:54 -0500

    completed stub profile component

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,17 @@
 import React from "react"
 
 export function Profile() {
+  // TODO: Retrieve user information
+
   return (
     <div>
       <h2>Profile</h2>
+      <p>User: usernameHere</p>
+      <p>User Since: userSinceHere</p>
+      <p>
+        Update Password
+        <span>inputElementHereToUpdatePassword</span>
+      </p>
     </div>
   )
 }
