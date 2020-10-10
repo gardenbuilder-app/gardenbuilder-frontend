1ae009dac3f79faca87a6ea0b1d0cdb9d92b5de7
David Thompson <capndavet@gmail.com>
2020-10-10 10:00:10 -0500

    removed unneccesary semicolons

--- src/pages/Gardens/AddGarden/AddGarden.test.js
+++ src/pages/Gardens/AddGarden/AddGarden.test.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import { render,  screen, waitFor } from '@testing-library/react'
-import { ApolloProvider } from '@apollo/client'
-import client from 'ApolloClient';
-import userEvent from '@testing-library/user-event';
-import { AddGarden } from './AddGarden'
+import React from "react"
+import { render, screen, waitFor } from "@testing-library/react"
+import { ApolloProvider } from "@apollo/client"
+import client from "ApolloClient"
+import userEvent from "@testing-library/user-event"
+import { AddGarden } from "./AddGarden"
 
-describe('<AddGarden/>', () => {
+describe("<AddGarden/>", () => {
   beforeEach(() => {
     render(
       <ApolloProvider client={client}>
-        <AddGarden/>
+        <AddGarden />
       </ApolloProvider>
     )
   })
 
-  it('renders correctly', async () => {
-    expect(await screen.findByText('Add Garden')).toBeInTheDocument();
+  it("renders correctly", async () => {
+    expect(await screen.findByText("Add Garden")).toBeInTheDocument()
   })
 
-  it('toggles the Add Garden form on click', async () => {
+  it("toggles the Add Garden form on click", async () => {
     const toggler = await screen.findByText(/Add Garden/i)
-    await userEvent.click(toggler);
-    expect(await screen.findByText(/Garden Name/i)).toBeInTheDocument();
-    await userEvent.click(toggler);
+    await userEvent.click(toggler)
+    expect(await screen.findByText(/Garden Name/i)).toBeInTheDocument()
+    await userEvent.click(toggler)
     await waitFor(() => {
-      expect(screen.queryByText(/Garden Name/i)).toBeNull();
+      expect(screen.queryByText(/Garden Name/i)).toBeNull()
     })
   })
 })
