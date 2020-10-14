f54bc0449ae7bf1dc75857e2afeea2842201acbe
Katherina Curley <paytonagreen@gmail.com>
2020-10-13 20:06:21 -0500

    <BedList /> basic test

--- src/pages/Garden/BedList/BedList.test.js
+++ src/pages/Garden/BedList/BedList.test.js
@@ -1,12 +1,14 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { BedList } from "./BedList"
 
 describe("<BedList /> component", () => {
-  const container = render(<BedList />)
+  beforeEach(() => {
+  render(<BedList />)
+  })
 
-  it("has no tests yet", () => {
-    console.warn("FAKE TEST WARNING: There are no tests here yet")
-    expect("No tests yet").toBe("No tests yet")
+  it("renders mock beds", async () => {
+    expect(await screen.findByText(/Mock Bed 0/i)).toBeInTheDocument();
+    expect(await screen.findByText(/Mock Bed 1/i)).toBeInTheDocument();
   })
 })
