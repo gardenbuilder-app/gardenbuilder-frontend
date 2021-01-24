cf7dda335e27053f0fcdb84f7d2a88412a1a0666
David Thompson <capndavet@gmail.com>
2021-01-23 20:15:12 -0600

    update Welcome page to use Center component

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,19 +1,15 @@
 import React from "react"
-import {Link} from 'react-router-dom'
-// import styled from 'styled-components'
-
-
-// const Center = styled.div`
-//   margin-left: 4rem;
-// `;
+import { Link } from 'react-router-dom'
+import { Center } from '../../components/style'
 
 export function Welcome() {
   return (
-    <div>
-      <h2>Welcome to Gardenbuilder</h2>
-      <p>In order to get started, please 
-          <Link to="/gardens">make a garden</Link> 
-      </p>
-    </div>
+    <Center>
+      <div>
+        <h2>Welcome to Gardenbuilder</h2>
+        <p>In order to get started, please <Link to="/gardens">make a garden</Link>
+        </p>
+      </div>
+    </Center>
   )
 }
