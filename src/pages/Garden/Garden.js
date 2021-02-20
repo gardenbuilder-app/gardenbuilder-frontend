30ba6ca78a0007599b03a9fce29f5b031a048200
David Thompson <capndavet@gmail.com>
2021-02-20 16:11:36 -0600

    use fake data in Garden paged

--- src/pages/Garden/Garden.js
+++ src/pages/Garden/Garden.js
@@ -1,15 +1,18 @@
 import React from "react"
 import { useQuery } from '@apollo/client';
+import { useUrlParam } from '../../hooks'
 import { GET_USER_GARDENS} from 'queries'
 import { BedList } from "./BedList"
 
 export function Garden() {
+  const gardenName = useUrlParam('name')
   const { data, loading, error } = useQuery(GET_USER_GARDENS);
+
   if (loading) return <p>Loading...</p>
   if (error) return <div className="error">{error.message}</div>
   if (data) return (
     <div>
-      <h2>Garden</h2>
+      <h2>{ gardenName }</h2>
       <BedList />
     </div>
   )
