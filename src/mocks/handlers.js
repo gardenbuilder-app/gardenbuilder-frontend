eb598a6a6e299d114bce5201e8bd83c396d29fc3
David Thompson <capndavet@gmail.com>
2021-02-20 16:09:27 -0600

    update userGardens mock to reflect current data structure

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -30,23 +30,30 @@ export const handlers = [
   graphql.query("GET_USER_GARDENS", (req, res, ctx) => {
     return res(
       ctx.data({
-        userGardens: [
-          {
-            id: "1",
-            name: "Garden One",
-            beds: [],
-            isActive: false,
-          },
-          {
-            id: "2",
-            name: "Garden Two",
-            beds: [
-              {id: 1},
-              {id: 2}
-            ],
-            isActive: true,
-          }
-        ]
+        gardens: {
+          gardens: [
+            {
+              id: "1",
+              name: "Garden One",
+              ownerId: 1,
+              endedAt: "2021-02-13T18:58:58.125Z",
+              isActive: false,
+              ownerId: 1,
+              createdAt: "2021-02-13T18:58:58.125Z",
+              updatedAt: "2021-02-13T18:58:58.125Z"
+            },
+            {
+              id: "2",
+              name: "Garden Two",
+              endedAt: "2021-02-13T18:58:58.125Z",
+              ownerId: 1,
+              isActive: true,
+              createdAt: "2021-02-13T18:58:58.125Z",
+              updatedAt: "2021-02-13T18:58:58.125Z"
+            }
+          ],
+          errors: []
+      }
       })
     )
   }),
